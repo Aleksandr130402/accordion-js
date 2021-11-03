54f91a07d3b38ef98feff09e6d32ed6f0061f11d
Sasha <o.stolitenko@temabit.com>
2021-11-03 13:23:55 +0200

    Using createElement and hung listeners on items

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const root = document.getElementById('root');
+const accordion = document.createElement('div');
 
-root.insertAdjacentHTML('afterbegin',
-'<div class="accordion" id="accordionExample"></div>'
-);
-
-const accordion = root.querySelector(".accordion");
+accordion.className = "accordion";
+accordion.id = "accordionExample";
+root.append(accordion);
 
 const methods = [
 	{
@@ -85,294 +84,346 @@ const methods = [
 	}
 ];
 
-let accordItems = methods.map((method) => {
-	return (
-		`<div class="accordion-item">
-			<h2 class="accordion-header" id="heading-${method.name}">
-				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#${method.name}"
-					aria-expanded="true" aria-controls="${method.name}">
-					${method.name === 'indexOf' ? 'indexOf / lastIndexOf' : method.name}
-				</button>
-			</h2>
-			<div id="${method.name}" class="accordion-collapse collapse" aria-labelledby="heading-${method.name}"
-				data-bs-parent="#accordionExample">
-				<div class="accordion-body row">
-					<p>${method.desc}</p>
-					<div class="row">
-						<div class="col-3">
-							<input class="field" value="1,2,3,4,5">
-						</div>
-						<div class="col-3">
-							<button class="opr">${method.name}</button>
-							${method.name === 'indexOf' ? '<button class="opr" id="lastIndexOf">lastIndexOf</button>' : ""}
-							<button class="reset">reset</button>
-						</div>
-						<div class="result"></div>
-					</div>
-				</div>
-			</div>
-		</div>`
-	)
+methods.forEach((method) => {
+
+	let accordionItem = document.createElement('div');
+	let accordionBody = document.createElement('div');
+	let accordionHeader = document.createElement('h2');
+	let accordionButton = document.createElement('button');
+	let accordionCollapse = document.createElement('div');
+	let p = document.createElement('p');
+	let row = document.createElement('div');
+	let colFirst = document.createElement('div');
+	let colSecond = document.createElement('div');
+	let field = document.createElement('input');
+	let opr = document.createElement('button');
+	let reset = document.createElement('button');
+	let result = document.createElement('div');
+
+	accordionItem.className = "accordion-item";
+	accordionBody.className = "accordion-body";
+	accordionHeader.className = "accordion-header";
+	accordionButton.className = "accordion-button";
+	accordionButton.type = "button";
+	accordionButton.setAttribute("data-bs-toggle", "collapse");
+	accordionButton.setAttribute("aria-expanded", "true");
+	accordionCollapse.className = "accordion-collapse collapse";
+	accordionCollapse.setAttribute("data-bs-parent", "#accordionExample");
+	opr.className = "opr";
+	reset.className = "reset";
+	reset.textContent = "reset";
+	colFirst.className = "col-3";
+	colSecond.className = "col-3";
+	field.className = "field";
+	field.value = "1,2,3,4,5";
+	row.className = "row";
+	result.className = "result";
+
+	accordionButton.setAttribute("data-bs-toggle", "collapse");
+	accordionButton.setAttribute("data-bs-target", `#${method.name}`);
+	accordionButton.setAttribute("aria-controls", `${method.name}`);
+	accordionButton.textContent = `${method.name === 'indexOf' ? 'indexOf / lastIndexOf' : method.name}`;
+	
+	accordionHeader.id = `heading-${method.name}`;
+	accordionHeader.append(accordionButton);
+
+	accordionCollapse.id = `${method.name}`;
+	accordionCollapse.setAttribute("aria-labelledby", `heading-${method.name}`);
+
+	accordionBody.classList.add("row");
+	p.textContent = `${method.desc}`;
+
+	colFirst.append(field);
+
+	opr.append(colFirst);
+	opr.textContent = `${method.name}`;
+	colSecond.append(opr, reset);
+
+	if(method.name === 'indexOf') {
+		const lastIndexOf = document.createElement('button');
+		lastIndexOf.className = "opr";
+		lastIndexOf.id = "lastIndexOf";
+		lastIndexOf.textContent = "lastIndexOf";
+		colSecond.querySelector(".opr").after(lastIndexOf)
+	}
+
+	row.append(colFirst, colSecond);
+
+	accordionBody.append(p, row, result);
+
+	accordionCollapse.append(accordionBody);
+
+	accordionItem.append(accordionHeader, accordionCollapse);
+	
+	accordion.append(accordionItem);
 });
 
-for (item of accordItems) {
-	accordion.insertAdjacentHTML('beforeend', item);
-}
+function createInput(item, name, placeholder, type) {
 
-function addHTML(elem, content) {
+	let input = document.createElement('input');
 
-	return elem.querySelector(".field").insertAdjacentHTML('afterend', content);
-
+	input.name = name;
+	input.placeholder = placeholder;
+	if(type === undefined) {
+		type = input.type;
+	} else {
+		input.type = type;
+	}
+	
+	item.querySelector("input:last-child").after(input);
 }
 
 const splice = document.getElementById('splice');
 
-addHTML(splice, `
-	<input name="start" placeholder="pos" type="number">
-	<input name="deleteCount" placeholder="delete count" type="number">
-	<input name="insertItems" placeholder="items">
-`);
+createInput(splice, "start", "pos", "number");
+createInput(splice, "deleteCount", "delete count", "number");
+createInput(splice, "insertItems", "items");
 
 const slice = document.getElementById('slice');
 
-addHTML(slice, `
-	<input name="start" placeholder="start" type="number">
-	<input name="end" placeholder="end" type="number">
-`);
+createInput(slice, "start", "start", "number");
+createInput(slice, "end", "end", "number");
 
 const concat = document.getElementById('concat');
 
-addHTML(concat, `
-	<input name="addItems" placeholder="items">
-`);
+createInput(concat, "addItems", "items");
 
 const indexOf = document.getElementById('indexOf');
 
-addHTML(indexOf, `
-	<input name="indexOfItem" placeholder="item">
-	<input name="pos" placeholder="pos">
-`);
+createInput(indexOf, "indexOfItem", "item");
+createInput(indexOf, "pos", "pos");
 
 const includes = document.getElementById('includes');
 
-addHTML(includes, `
-	<input name="includesValue" placeholder="value">
-`);
+createInput(includes, "includesValue", "value");
 
 const find = document.getElementById('find');
 
-addHTML(find, `
-	<input name="findValue" placeholder="value">
-`);
+createInput(find, "findValue", "value");
 
 const filter = document.getElementById('filter');
 
-addHTML(filter, `
-	<input name="filterValue" placeholder="value">
-`);
+createInput(filter, "filterValue", "value");
 
 const findIndex = document.getElementById('findIndex');
 
-addHTML(findIndex, `
-	<input name="findIndexValue" placeholder="value">
-`);
+createInput(findIndex, "findIndexValue", "value");
 
 const split = document.getElementById('split');
 
-addHTML(split, `
-	<input name="separator" placeholder="separator">
-`);
+createInput(split, "separator", "separator");
 
 const join = document.getElementById('join');
 
-addHTML(join, `
-	<input name="separator" placeholder="separator">
-`);
+createInput(join, "separator", "separator");
 
 let mainInput;
 let inputs;
 let result;
 
-accordion.onclick = function(e) {
+const accordButtons = document.getElementsByClassName('accordion-button');
+const fields = document.getElementsByClassName('field');
+const buttons = document.getElementsByClassName('opr');
+const resets = document.getElementsByClassName('reset');
 
-	if(e.target.className === 'accordion-button') {
+for (let accordButton of accordButtons) {
 
-		let accordItem = e.target.closest('.accordion-item');
-		let accordBody = accordItem.querySelector('.accordion-body');
-		//получить все инпуты выбранного в аккордионе метода
-		inputs = accordBody.getElementsByTagName('input');
-		mainInput = accordBody.querySelector('.field');
-		result = accordBody.querySelector('.result');
+	accordButton.onclick = function(e) {
+		accordButtonCliked(e);
 	}
+} 
 
-	//отследить нажатие кнопки метода
-	if(e.target.className === 'opr') {
+for (let field of fields) {
 
-		let array = [];
-		let buttonName = e.target.outerText;
-		let value = mainInput.value;
+	field.onclick = function() {
+		field.onkeydown = function(e) {
 
-		function splitValue() {
-			return value.split(',');
-		}
-
-		if (result.innerText !== "") 
-			array = result.innerText.split(',');
-
-		if (buttonName !== "push" && buttonName !== "unshift")
-			array = splitValue();
-
-		switch(buttonName) {
-			
-			case 'push': 
-				array.push(splitValue());
-				break;
-
-			case 'pop':
-				array.pop();
-				break;
-
-			case 'unshift':
-				array.unshift(splitValue());
-				break;
-
-			case 'shift':
-				array.shift();
-				break;
-
-			case 'splice':
-
-				let spliceStart = inputs.start.value;
-				let deleteCount = inputs.deleteCount.value;
-				let insertItems = inputs.insertItems.value;
-
-				array.splice(spliceStart, deleteCount, insertItems);
-				break;
-
-			case 'slice':
-
-				let sliceStart = inputs.start.value;
-				let sliceEnd = inputs.end.value;
-
-				array = array.slice(sliceStart, sliceEnd);
-				break;
-
-			case 'concat': 
-
-				let items = inputs.addItems.value;
-
-				array = array.concat(items);
-				break;
-
-			case 'indexOf':	
-
-				let indexOfItem = inputs.indexOfItem.value;
-				let indexOfPos = inputs.pos.value;
-
-				array = array.indexOf(indexOfItem, indexOfPos);
-				break;
-
-			case 'lastIndexOf':
-				
-				let lastIndexOfItem = inputs.indexOfItem.value;
-				let lastIndexOfPos = inputs.pos.value;
-
-				array = array.lastIndexOf(lastIndexOfItem, lastIndexOfPos);
-				break;
-
-			case 'includes':
-
-				let includesValue = inputs.includesValue.value;
-
-				array = array.includes(includesValue);
-				break;
-
-			case 'find':
-
-				let findValue = inputs.findValue.value;
-
-				array = array.find(i => i === findValue);
-				break;
-
-			case 'filter':
-
-				let filterValue = inputs.filterValue.value;
-
-				array = array.filter(i => i === filterValue);
-				break;
-
-			case 'findIndex':
-
-				let findIndexValue = inputs.findIndexValue.value;
-
-				array = array.findIndex(i => i === findIndexValue);
-				break;
-
-			case 'forEach':
-
-				array.forEach(i => {
-					alert("item "+i+" on position "+ array.indexOf(i));
-				});
-				break;
-
-			case 'map':
-
-				array = array.map(i => i.length);
-				break;
-
-			case 'sort': 
-
-				array = array.sort((a, b) => a - b);
-				break;
-
-			case 'reverse': 
-
-				array = array.reverse();
-				break;
-
-			case 'split':
-
-				let str = array.toString();
-				let splitSep = inputs.separator.value;
-				
-				array = str.split(splitSep);
-				break;
-
-			case 'join':
-
-				let joinSep = inputs.separator.value;
-
-				array = array.join(joinSep);
-				break;
-
-			case 'reduce':
-
-				array = array.reduce((prev, current) => +prev + +current);
-				break;
-
-			default: alert('no such method!');
-		}
-
-		result.innerText = array;
-	}
-
-	//основное поле ввода
-	if(e.target.className === 'field') {
-
-		mainInput.onchange = function(e) {
-
-			if (mainInput.key === ' '){
+			if (e.key === ' '){
 				e.preventDefault();
 			};
 		}	
 	}
+} 
 
-	if(e.target.className === 'reset') {
+for (let button of buttons) {
 
-		for(let input of inputs) {
-			input.value = '';
-		}
-
-		result.innerText = '';
+	button.onclick = function(e) {
+		oprCliked(e);
 	}
+} 
+
+for (let reset of resets) {
+	reset.onclick = function() {
+		resetClicked();
+	}
+}
+
+function accordButtonCliked(e) {
+
+	let accordItem = e.target.closest('.accordion-item');
+	let accordBody = accordItem.querySelector('.accordion-body');
+	//получить все инпуты выбранного в аккордионе метода
+	inputs = accordBody.getElementsByTagName('input');
+	mainInput = accordBody.querySelector('.field');
+	result = accordBody.querySelector('.result');
+}
+
+function oprCliked(e) {
+
+	let array = [];
+	let buttonName = e.target.outerText;
+	let value = mainInput.value;
+
+	function splitValue() {
+		return value.split(',');
+	}
+
+	if (result.innerText !== "") 
+		array = result.innerText.split(',');
+
+	if (buttonName !== "push" && buttonName !== "unshift")
+		array = splitValue();
+
+	switch(buttonName) {
+		
+		case 'push': 
+			array.push(splitValue());
+			break;
+
+		case 'pop':
+			array.pop();
+			break;
+
+		case 'unshift':
+			array.unshift(splitValue());
+			break;
+
+		case 'shift':
+			array.shift();
+			break;
+
+		case 'splice':
+
+			let spliceStart = inputs.start.value;
+			let deleteCount = inputs.deleteCount.value;
+			let insertItems = inputs.insertItems.value;
+
+			array.splice(spliceStart, deleteCount, insertItems);
+			break;
+
+		case 'slice':
+
+			let sliceStart = inputs.start.value;
+			let sliceEnd = inputs.end.value;
+
+			array = array.slice(sliceStart, sliceEnd);
+			break;
+
+		case 'concat': 
+
+			let items = inputs.addItems.value;
+
+			array = array.concat(items);
+			break;
+
+		case 'indexOf':	
+
+			let indexOfItem = inputs.indexOfItem.value;
+			let indexOfPos = inputs.pos.value;
+
+			array = array.indexOf(indexOfItem, indexOfPos);
+			break;
+
+		case 'lastIndexOf':
+			
+			let lastIndexOfItem = inputs.indexOfItem.value;
+			let lastIndexOfPos = inputs.pos.value;
+
+			array = array.lastIndexOf(lastIndexOfItem, lastIndexOfPos);
+			break;
+
+		case 'includes':
+
+			let includesValue = inputs.includesValue.value;
+
+			array = array.includes(includesValue);
+			break;
+
+		case 'find':
+
+			let findValue = inputs.findValue.value;
+
+			array = array.find(i => i === findValue);
+			break;
+
+		case 'filter':
+
+			let filterValue = inputs.filterValue.value;
+
+			array = array.filter(i => i === filterValue);
+			break;
+
+		case 'findIndex':
+
+			let findIndexValue = inputs.findIndexValue.value;
+
+			array = array.findIndex(i => i === findIndexValue);
+			break;
+
+		case 'forEach':
+
+			array.forEach(i => {
+				alert("item "+i+" on position "+ array.indexOf(i));
+			});
+			break;
+
+		case 'map':
+
+			array = array.map(i => i.length);
+			break;
+
+		case 'sort': 
+
+			array = array.sort((a, b) => a - b);
+			break;
+
+		case 'reverse': 
+
+			array = array.reverse();
+			break;
+
+		case 'split':
+
+			let str = array.toString();
+			let splitSep = inputs.separator.value;
+			
+			array = str.split(splitSep);
+			break;
+
+		case 'join':
+
+			let joinSep = inputs.separator.value;
+
+			array = array.join(joinSep);
+			break;
+
+		case 'reduce':
+
+			array = array.reduce((prev, current) => +prev + +current);
+			break;
+
+		default: alert('no such method!');
+	}
+
+	if(buttonName !== 'forEach')
+		result.innerText = array;
+}
+
+function resetClicked() {
+
+	for(let input of inputs) {
+		input.value = '';
+	}
+
+	result.innerText = '';	
 }
